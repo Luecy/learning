c10f1bf552a88ca27241c7ff1a57f5a93616cf11
Luecy <taaffeite76@gmail.com>
2026-02-28 13:25:28 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = "Github_main_v1";
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil((async () => {
+		await clients.claim();
+	})``);
+});
+
+self.addEventListener("fetch", async (event) => {
+	event.respondWith((async () => {
+		const request = event.request;
+		let response;
+		try {
+			const cache = await caches.open(cacheName);
+			response = await cache.match(request);
+			if (!response) {
+				await cache.add(request);
+				response = await cache.match(request);
+			}
+		} catch {
+			response = await fetch(request);
+		}
+		return response;
+	})``);
+});
